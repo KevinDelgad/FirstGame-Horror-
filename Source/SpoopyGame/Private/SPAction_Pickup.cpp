0c961ae3ed0963bbdf4d8e5706417d95b66bf898
KevinDelgad <54679846+KevinDelgad@users.noreply.github.com>
2025-03-20 18:48:05 -0700

    PickUpAction Updates
    - Convert Pickup from Interact Component to Action Component
    - Added Tick to PickUp Action

--- Source/SpoopyGame/Private/SPAction_Pickup.cpp
+++ Source/SpoopyGame/Private/SPAction_Pickup.cpp
@@ -4,12 +4,54 @@
 #include "SPAction_Pickup.h"
 
 #include "SPCharacter.h"
+#include "SPPickupBase.h"
 #include "Camera/CameraComponent.h"
 
 USPAction_Pickup::USPAction_Pickup()
 {
 	ReachDistance = 100.0f;
 	TraceRadius = 20.0f;
+}
+
+void USPAction_Pickup::Tick(float DeltaTime)
+{
+	if (LastFrameNumberWeTicked == GFrameCounter)
+		return;
+
+
+	BPTick(DeltaTime);
+	
+	LastFrameNumberWeTicked = GFrameCounter;
+}
+
+bool USPAction_Pickup::IsTickable() const
+{
+	return bHolding;
+}
+
+AActor* USPAction_Pickup::CreateActorClone(AActor* Instigator, TSubclassOf<AActor> PickUpObj, FTransform PickupTransform)
+{
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	SpawnParams.Instigator = Cast<APawn>(Instigator);
+	
+	return GetWorld()->SpawnActor<AActor>(PickUpObj, PickupTransform, SpawnParams);
+}
+
+FHitResult USPAction_Pickup::GetHitResult(const ASPCharacter* InstigatorCharacter)
+{
+	FHitResult HitResult;
+
+	FVector EyeLocation = InstigatorCharacter->FindComponentByClass<UCameraComponent>()->GetComponentLocation();
+	FRotator EyeRotation = InstigatorCharacter->GetControlRotation();
+
+	FVector End = EyeLocation + (EyeRotation.Vector() * ReachDistance);
+
+	FName ProfileToBlock = "BlockAllStatic";
+	
+	bool bBlockingHit = GetWorld()->LineTraceSingleByProfile(HitResult, EyeLocation, End, ProfileToBlock);
+	
+	return HitResult;
 }
 
 bool USPAction_Pickup::SetHitTarget(const ASPCharacter* InstigatorCharacter)
@@ -49,7 +91,7 @@ void USPAction_Pickup::StartAction_Implementation(AActor* Instigator)
 
 	if (Character)
 	{
-		if (SetHitTarget(Character))
+		if (!SetHitTarget(Character))
 		{
 			return;
 		}
--- Source/SpoopyGame/Public/SPAction_Pickup.h
+++ Source/SpoopyGame/Public/SPAction_Pickup.h
@@ -6,16 +6,25 @@
 #include "SPAction.h"
 #include "SPAction_Pickup.generated.h"
 
+class ASPPickupBase;
 class ASPCharacter;
 /**
  * 
  */
 UCLASS()
-class SPOOPYGAME_API USPAction_Pickup : public USPAction
+class SPOOPYGAME_API USPAction_Pickup : public USPAction , public FTickableGameObject
 {
 	GENERATED_BODY()
 
+	private:
+	// The last frame number we were ticked.
+	// We don't want to tick multiple times per frame 
+	uint32 LastFrameNumberWeTicked = INDEX_NONE;
+	
 	protected:
+
+	UFUNCTION(BlueprintCallable, Category = "PickUp")
+	AActor* CreateActorClone( AActor* Instigator ,TSubclassOf<AActor> PickUpObj, FTransform PickupTransform);
 
 	UPROPERTY(BlueprintReadOnly)
 	AActor* HitTarget;
@@ -28,6 +37,9 @@ class SPOOPYGAME_API USPAction_Pickup : public USPAction
 
 	UPROPERTY(EditDefaultsOnly)
 	float TraceRadius;
+
+	UPROPERTY(BlueprintReadWrite)
+	bool bHolding = false;
 	
 	UPROPERTY(EditDefaultsOnly)
 	TEnumAsByte<ECollisionChannel> HitChannel;
@@ -35,8 +47,28 @@ class SPOOPYGAME_API USPAction_Pickup : public USPAction
 	
 	virtual void StartAction_Implementation(AActor* Instigator) override;
 
+	UFUNCTION(BlueprintCallable, Category = "Pickup")
+	FHitResult GetHitResult(const ASPCharacter* InstigatorCharacter);
+	
+	
 	//virtual void EndAction_Implementation() override;
-
+	
 public:
 	USPAction_Pickup();
+
+	UFUNCTION(BlueprintCallable, Category = "Pickup")
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual ETickableTickType GetTickableTickType() const override
+	{
+		return ETickableTickType::Conditional;
+	}
+	virtual TStatId GetStatId() const override
+	{
+		RETURN_QUICK_DECLARE_CYCLE_STAT(MyTickableClass, STATGROUP_Tickables);
+	}
+	virtual bool IsTickable() const override;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Pickup")
+	void BPTick(float DeltaTime);
 };
